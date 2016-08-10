1717bb672d9b07a699684aaaf30716382df35988
Eddy Ntambwe <eddydarell@gmail.com>
2016-08-10 13:59:02 +0200

    Add project files.

---
+++ LMS_Grupp4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMS_Grupp4.Startup))]
+namespace LMS_Grupp4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
