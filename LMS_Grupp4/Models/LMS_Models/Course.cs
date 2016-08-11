27606dfa83e8154c0378686d8a3c6eb37f11c295
Marla969 <markus.landberg@hotmail.com>
2016-08-11 12:51:18 +0200

    Changes in Assignment and Course

--- LMS_Grupp4/Models/LMS_Models/Course.cs
+++ LMS_Grupp4/Models/LMS_Models/Course.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LMS_Grupp4.Models.LMS_Models
 {
     public class Course
     {
+        [Key]
+        public int ID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseTeacher { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<ClassSchema> ClassSchemes { get; set; }
+        public virtual ICollection<Class> Classes { get; set; }
+        public virtual ICollection<Assignment> Assignments { get; set; }
+        public virtual ICollection<File> Files { get; set; }
     }
 }
