49d15662594f59b982adccb6b5008741729b1e66
Chrille80 <chr.wiren@gmail.com>
2016-08-11 10:08:00 +0200

    Removed applicationinsights file to be able to interpret data annotations

--- LMS_Grupp4/Models/LMS_Models/File.cs
+++ LMS_Grupp4/Models/LMS_Models/File.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LMS_Grupp4.Models.LMS_Models
 {
 	public class File
 	{
+		[Key]
+		public int FileID { get; set; }
 	}
 }
