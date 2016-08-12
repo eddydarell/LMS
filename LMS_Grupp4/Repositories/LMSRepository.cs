3017d275bb2807a16eb07b7f641c21c8807943c0
Marla969 <markus.landberg@hotmail.com>
2016-08-12 10:42:35 +0200

    Add Course to LMSRepository

--- LMS_Grupp4/Repositories/LMSRepository.cs
+++ LMS_Grupp4/Repositories/LMSRepository.cs
@@ -14,7 +14,8 @@ namespace LMS_Grupp4.Repositories
 		/// Database connections
 		private ApplicationDbContext db = new ApplicationDbContext();
 
-		// Returns all assignments from the database
+        #region Assignment
+        // Returns all assignments from the database
 		public IEnumerable<Assignment> GetAllAssignments()
 		{
 			return db.Assignments.ToList();
@@ -27,26 +28,63 @@ namespace LMS_Grupp4.Repositories
 		}
 
 		// Adds assignments to the database
-		public void Add(Assignment assignment)
+		public void AddAssignment(Assignment assignment)
 		{
 			db.Assignments.Add(assignment);
 			db.SaveChanges();
 		}
 
 		// Edits an assignment in the database
-		public void Edit(Assignment assignment)
+		public void EditAssignment(Assignment assignment)
 		{
 			db.Entry(assignment).State = EntityState.Modified;
 			db.SaveChanges();
 		}
 
-		// Deletes an assignments from the database
-		public void Delete(int id)
+		// Deletes a assignment from the database
+		public void DeleteAssignment(int id)
 		{
 			Assignment assignment = db.Assignments.Find(id);
 			db.Assignments.Remove(assignment);
 			db.SaveChanges();
-		}
+        }
+        #endregion Assignment
 
-	}
+        #region Course
+        // Returns all courses from the database
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return db.Courses.ToList();
+        }
+
+        public Course GetSpecificCourse(int id)
+        {
+            if (id != null)
+            { return db.Courses.Find(id); }
+            else return null;
+        }
+
+        // Adds courses to the database
+        public void AddCourse(Course course)
+        {
+            db.Courses.Add(course);
+            db.SaveChanges();
+        }
+
+        // Edits a course in the database
+        public void EditCourse(Course course)
+        {
+            db.Entry(course).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        // Deletes a course from the database
+        public void DeleteCourse(int id)
+        {
+            Course course = db.Courses.Find(id);
+            db.Courses.Remove(course);
+            db.SaveChanges();
+        }
+        #endregion Course
+    }
 }
