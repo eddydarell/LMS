00f15945e5e271986d842d9ad2afcfa3e89ef358
Eddy Ntambwe <eddydarell@gmail.com>
2016-08-16 16:02:12 +0200

    - Added: Migrations - Created: CourseApplication Model, _forbidden views, Apply view, courseApplication relationships

--- LMS_Grupp4/Migrations/201608161125018_AddedCreationDateToCourseModel.cs
+++ LMS_Grupp4/Migrations/201608161125018_AddedCreationDateToCourseModel.cs
@@ -3,14 +3,16 @@ namespace LMS_Grupp4.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class testadmin : DbMigration
+    public partial class AddedCreationDateToCourseModel : DbMigration
     {
         public override void Up()
         {
+            AddColumn("dbo.Courses", "CreationDate", c => c.DateTime(nullable: false));
         }
         
         public override void Down()
         {
+            DropColumn("dbo.Courses", "CreationDate");
         }
     }
 }
