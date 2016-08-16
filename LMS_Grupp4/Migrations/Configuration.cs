d32fc71aed69fbe8c86700a59b6b4299f466db04
Marla969 <markus.landberg@hotmail.com>
2016-08-16 11:29:18 +0200

    Misspelled assignment in IdentityModels

--- LMS_Grupp4/Migrations/Configuration.cs
+++ LMS_Grupp4/Migrations/Configuration.cs
@@ -226,6 +226,9 @@ namespace LMS_Grupp4.Migrations
 
             //course1.Users.Add(teacherUser);
             teacherUser.Courses.Add(course1);
+            teacherUser.Assignments.Add(assignment1);
+            teacherUser.ClassSchemas.Add(classShema1);
+            teacherUser.ProgramClasses.Add(programClass1);
             context.SaveChanges();
         }
     }
