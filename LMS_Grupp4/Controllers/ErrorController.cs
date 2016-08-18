07872e8ccfd147979304b73ae8ca68218398071e
Marla969 <markus.landberg@hotmail.com>
2016-08-18 16:02:35 +0200

    Add ErrorController

---
+++ LMS_Grupp4/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS_Grupp4.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
