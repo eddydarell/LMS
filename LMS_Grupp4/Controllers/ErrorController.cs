373a7f7c401a17995871dcccef369231c92350c1
Marla969 <markus.landberg@hotmail.com>
2016-08-19 14:46:54 +0200

    First version ErrorController and ErrorViews

--- LMS_Grupp4/Controllers/ErrorController.cs
+++ LMS_Grupp4/Controllers/ErrorController.cs
@@ -8,10 +8,32 @@ namespace LMS_Grupp4.Controllers
 {
     public class ErrorController : Controller
     {
-        // GET: Error
-        public ActionResult Index()
+     
+        // GET: Custom Errors      
+        public ActionResult ErrorHandler400()
+        {
+            return View();
+        }
+
+        public ActionResult ErrorHandler401()
+        {
+            return View();
+        }
+
+        public ActionResult ErrorHandler403()
+        {
+            return View();
+        }
+
+        public ActionResult ErrorHandler404()
+        {
+            return View();
+        }
+
+        public ActionResult ErrorHandler500()
         {
             return View();
         }
     }
+
 }
