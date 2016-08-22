d8dcceb9a2111c64206ecc7081e881359a7a0f9f
Chrille80 <chr.wiren@gmail.com>
2016-08-22 15:58:13 +0200

    New commit after accidently reverting

--- LMS_Grupp4/Controllers/AssignmentController.cs
+++ LMS_Grupp4/Controllers/AssignmentController.cs
@@ -6,17 +6,18 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using LMS_Grupp4.Repositories;
+using LMS_Grupp4.Models.LMS_ViewModels;
+using LMS_Grupp4.Models.LMS_Models;
 
 namespace LMS_Grupp4.Controllers
 {
-	[Authorize(Roles = "student, teacher")]
-    public class AssignmentController : Controller
-    {
-        LMSRepository LMSRepo = new LMSRepository();
+	public class AssignmentController : Controller
+	{
+		LMSRepository LMSRepo = new LMSRepository();
 
 
-        public ActionResult Index(string id = "")
-        {
+		public ActionResult Index(string id = "")
+		{
 			if (String.IsNullOrWhiteSpace(id))
 			{
 				id = User.Identity.GetUserId();
@@ -26,8 +27,32 @@ namespace LMS_Grupp4.Controllers
 			var user = LMSRepo.GetUserManager().FindById(id);
 			var assignmentModelList = user.Assignments.ToList();
 
-            return View(assignmentModelList);
-        }
+			return View(assignmentModelList);
+		}
+
+		[Authorize(Roles = "teacher")]
+		[HttpGet]
+		public ActionResult Create(int courseID = 0)
+		{
+			//var students = LMSRepo.GetRoleManager().FindByName("student").Users.ToList();
+			var roleManager = LMSRepo.GetRoleManager();
+			var studentRole = roleManager.FindByName("student");
+			var course = LMSRepo.GetCourseByID(courseID);
+			var students = course.Users.Where(stu => stu.Roles.FirstOrDefault(r => r.RoleId == studentRole.Id) != null).ToList();
+
+
+			Assignment_CreateViewModel a_CVM = new Assignment_CreateViewModel(students);
+
+			return View(a_CVM);
+		}
+
+		[Authorize(Roles = "teacher")]
+		[HttpPost]
+		public ActionResult Create([Bind(Include = "Name, DueDate, Percentage, MaxScore, Course, Student")] Assignment assignment)
+		{
+			return RedirectToAction("Index");
+			//return View();
+		}
 
 		public ActionResult Details(int? id)
 		{
@@ -35,5 +60,5 @@ namespace LMS_Grupp4.Controllers
 
 			return View(assignment);
 		}
-    }
+	}
 }
